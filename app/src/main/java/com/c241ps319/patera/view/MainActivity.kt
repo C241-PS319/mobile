2b485fa51e23d40e71dea95bc2b4175ef4c25b1f
Vicky Maulana <muhammad.vicky@ui.ac.id>
2024-06-02 23:55:04 +0700

    refactor: main activity path

--- app/src/main/java/com/c241ps319/patera/view/MainActivity.kt
+++ app/src/main/java/com/c241ps319/patera/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.c241ps319.patera
+package com.c241ps319.patera.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.c241ps319.patera.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
