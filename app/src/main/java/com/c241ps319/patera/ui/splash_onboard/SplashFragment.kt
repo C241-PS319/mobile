5e7d70afda351ba56f53559ee6a080b9f62d843a
lutfialdrii <lutfialdripermana@gmail.com>
2024-06-11 07:23:40 +0700

    feat: add sharedpref and setup font styling

--- app/src/main/java/com/c241ps319/patera/ui/splash_onboard/SplashFragment.kt
+++ app/src/main/java/com/c241ps319/patera/ui/splash_onboard/SplashFragment.kt
@@ -1,5 +1,7 @@
 package com.c241ps319.patera.ui.splash_onboard
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -8,6 +10,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.c241ps319.patera.MainActivity
 import com.c241ps319.patera.R
 
 class SplashFragment : Fragment() {
@@ -19,10 +22,22 @@ class SplashFragment : Fragment() {
 
         // Splash
         Handler(Looper.getMainLooper()).postDelayed({
-            findNavController().navigate(R.id.action_splashFragment_to_onboardFragment)
+            if (onBoardingIsFinished()) {
+                val intent = Intent(requireContext(), MainActivity::class.java)
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                startActivity(intent)
+            } else {
+                findNavController().navigate(R.id.action_splashFragment_to_onboardFragment)
+            }
         }, 3000)
 
         return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    private fun onBoardingIsFinished(): Boolean {
+        // SharedPref
+        val sharedPreferences = requireActivity().getSharedPreferences("onBoarding", Context.MODE_PRIVATE)
+        return sharedPreferences.getBoolean("finished",false)
     }
 
 }
