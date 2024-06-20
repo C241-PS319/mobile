af5894c72d02459fbc54e9c6a72f464e4a458dfa
Vicky Maulana <muhammad.vicky@ui.ac.id>
2024-06-20 18:02:52 +0700

    feat: implement firebase authentication

--- app/src/main/java/com/c241ps319/patera/ui/auth/register/RegisterActivity.kt
+++ app/src/main/java/com/c241ps319/patera/ui/auth/register/RegisterActivity.kt
@@ -2,6 +2,7 @@ package com.c241ps319.patera.ui.auth.register
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -9,15 +10,37 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.credentials.CredentialManager
+import androidx.credentials.CustomCredential
+import androidx.credentials.GetCredentialRequest
+import androidx.credentials.GetCredentialResponse
+import androidx.credentials.exceptions.GetCredentialException
+import androidx.lifecycle.lifecycleScope
 import com.c241ps319.patera.R
 import com.c241ps319.patera.data.ResultState
+import com.c241ps319.patera.data.model.UserModel
 import com.c241ps319.patera.databinding.ActivityRegisterBinding
 import com.c241ps319.patera.ui.ViewModelFactory
 import com.c241ps319.patera.ui.auth.AuthViewModel
 import com.c241ps319.patera.ui.auth.RegisterSuccessActivity
+import com.c241ps319.patera.ui.auth.login.LoginActivity
+import com.c241ps319.patera.ui.auth.login.LoginActivity.Companion
+import com.c241ps319.patera.ui.main.MainActivity
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+import com.google.android.libraries.identity.googleid.GoogleIdTokenParsingException
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.coroutines.launch
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
+
+    private lateinit var auth: FirebaseAuth
 
     private val viewModel by viewModels<AuthViewModel> {
         ViewModelFactory.getInstance(this)
@@ -27,6 +50,9 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         supportActionBar?.hide();
+
+        auth = viewModel.auth
+
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -36,7 +62,7 @@ class RegisterActivity : AppCompatActivity() {
         }
 
         binding.btnRegisterGoogle.setOnClickListener {
-            showToast("Fitur Belum Tersedia")
+            googleSignIn()
         }
 
         binding.tvLogin.setOnClickListener {
@@ -95,6 +121,113 @@ class RegisterActivity : AppCompatActivity() {
                         }
                     }
                 }
+            }
+        }
+    }
+
+    private fun googleSignIn() {
+
+        val apiAvailability = GoogleApiAvailability.getInstance()
+        val resultCode = apiAvailability.isGooglePlayServicesAvailable(this)
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (apiAvailability.isUserResolvableError(resultCode)) {
+                apiAvailability.getErrorDialog(this, resultCode, 1)!!
+                    .show()
+            } else {
+                Log.i("Sign-In", "This device is not supported.")
+                finish()
+            }
+        }
+
+        val credentialManager = CredentialManager.create(this) //import from androidx.CredentialManager
+        val googleIdOption = GetGoogleIdOption.Builder()
+            .setFilterByAuthorizedAccounts(false)
+            .setServerClientId(getString(R.string.web_client_id))
+            .build()
+        val request = GetCredentialRequest.Builder() //import from androidx.CredentialManager
+            .addCredentialOption(googleIdOption)
+            .build()
+        lifecycleScope.launch {
+            try {
+                val result: GetCredentialResponse = credentialManager.getCredential(
+                    //import from androidx.CredentialManager
+                    request = request,
+                    context = this@RegisterActivity,
+                )
+                handleSignIn(result)
+            } catch (e: GetCredentialException) { //import from androidx.CredentialManager
+                Log.d("Error", e.message.toString())
+            }
+        }
+    }
+
+    private fun handleSignIn(result: GetCredentialResponse) {
+        // Handle the successfully returned credential.
+        when (val credential = result.credential) {
+            is CustomCredential -> {
+                if (credential.type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL) {
+                    try {
+                        val googleIdTokenCredential = GoogleIdTokenCredential.createFrom(credential.data)
+                        firebaseAuthWithGoogle(googleIdTokenCredential.idToken)
+                    } catch (e: GoogleIdTokenParsingException) {
+                        Log.e(TAG, "Received an invalid google id token response", e)
+                    }
+                } else {
+                    // Catch any unrecognized custom credential type here.
+                    Log.e(TAG, "Unexpected type of credential")
+                }
+            }
+
+            else -> {
+                // Catch any unrecognized credential type here.
+                Log.e(TAG, "Unexpected type of credential")
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential: AuthCredential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user: FirebaseUser? = auth.currentUser
+                    updateUI(user)
+                } else {
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?) {
+        if (currentUser != null) {
+            val userModel = UserModel(
+                name = currentUser.displayName ?: "",
+                email = currentUser.email ?: "",
+                phone = currentUser.phoneNumber ?: "",
+                picture = currentUser.photoUrl.toString(),
+                token = "",  // TODO: get token from firebase
+                isLogin = true,
+                isGoogleLogin = true
+            )
+
+            viewModel.saveSession(userModel)
+
+            val intent = Intent(this@RegisterActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        viewModel.getSession().observe(this) { session ->
+            if (session != null) {
+                val currentUser = auth.currentUser
+                updateUI(currentUser)
+            } else {
+                Log.d(TAG, "onStart: Session is null")
             }
         }
     }
