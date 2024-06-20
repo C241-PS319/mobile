7868c3f8fcf3edb8679c821d48dae814215b79ae
Vicky Maulana <muhammad.vicky@ui.ac.id>
2024-06-20 18:02:35 +0700

    feat: add firebase auth instance in authViewModel

--- app/src/main/java/com/c241ps319/patera/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/c241ps319/patera/ui/auth/AuthViewModel.kt
@@ -1,12 +1,17 @@
 package com.c241ps319.patera.ui.auth
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.c241ps319.patera.data.model.UserModel
 import com.c241ps319.patera.data.repository.PateraRepository
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.launch
 
 class AuthViewModel(private val pateraRepository: PateraRepository) : ViewModel() {
+
+    var auth: FirebaseAuth = FirebaseAuth.getInstance()
 
     fun register(name: String, email: String, password: String) =
         pateraRepository.register(name, email, password)
@@ -18,6 +23,10 @@ class AuthViewModel(private val pateraRepository: PateraRepository) : ViewModel(
 
     fun saveSession(userModel: UserModel) = viewModelScope.launch {
         pateraRepository.saveSession(userModel)
+    }
+
+    fun getSession(): LiveData<UserModel?> {
+        return pateraRepository.session.asLiveData()
     }
 
 }
