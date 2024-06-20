caddb3ac6694a5145ec79d052ed3d709ca399ad3
lutfialdrii <lutfialdripermana@gmail.com>
2024-06-20 22:16:03 +0700

    fix : button back

--- app/src/main/java/com/c241ps319/patera/ui/profile/about/AboutUsActivity.kt
+++ app/src/main/java/com/c241ps319/patera/ui/profile/about/AboutUsActivity.kt
@@ -6,13 +6,23 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.c241ps319.patera.R
+import com.c241ps319.patera.databinding.ActivityAboutUsBinding
 
 class AboutUsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAboutUsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_about_us)
+        binding = ActivityAboutUsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Set click listener on the back button
+        binding.btnBack.setOnClickListener {
+            finish()
+        }
         supportActionBar?.hide()
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
